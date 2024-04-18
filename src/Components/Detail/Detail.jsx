7d51c775834623742e6967510634f4a0060ba724
worney87 <151604385+worney87@users.noreply.github.com>
2024-04-18 18:37:14 +0900

    add detailHead

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import DetailHead from "./DetailHead";
+import DetailBody from "./DetailBody";
 
-function Detail({data}) {
+const DetailWrapper = styled.div`
+  margin: 60px auto 0;
+  max-width: 1200px;
+  width: 80%;
+`;
 
-    const {pId} = useParams();
-    console.log(pId)
+function Detail({ data }) {
+  const { pId } = useParams();
 
-    const product = data.filter(obj => obj.id === parseInt(pId));
+  const product = data.filter((obj) => obj.id === parseInt(pId));
 
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <DetailWrapper>
+      <DetailHead data={product}/>
+      <DetailBody detail={product.detail}/>
+    </DetailWrapper>
+  );
 }
 
 export default Detail;
--- src/Components/Detail/DetailHead.jsx
+++ src/Components/Detail/DetailHead.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import DescriptionList from "./DescriptionList";
+import styled from "styled-components";
 
-function DetailHead(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+const DetailHeadWrapper = styled.div`
+  max-width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const ProductImg = styled.img`
+  margin: auto;
+  width: 100%;
+`;
+
+function DetailHead({ data }) {
+  const product = data[0];
+
+  return (
+    <DetailHeadWrapper>
+      <ProductImg src={`/images/product/${product.main}`} alt="제품사진" />
+      <DescriptionList data={product} />
+    </DetailHeadWrapper>
+  );
 }
 
 export default DetailHead;
